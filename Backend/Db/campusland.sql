4829ef56a783aa63016a5dc4a4c54ee6f2f8e75b
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-06-28 11:11:37 -0500

    feat (Backend) : :sparkles: Creacion Bd y conexion de la misma
    Creacion de la base de datos junto con la conexión con pdo a la misma

---
+++ Backend/Db/campusland.sql
@@ -0,0 +1,50 @@
+/*Creación de la Base de Datos*/
+CREATE DATABASE campuslands;
+
+/*Usar la base de datos*/
+USE campuslands;
+
+/*Dropear las tablas si ya existen*/
+DROP TABLE IF EXISTS campers;
+DROP TABLE IF EXISTS region;
+DROP TABLE IF EXISTS departamento;
+DROP TABLE IF EXISTS pais;
+
+/*Creación de las tablas*/
+CREATE TABLE pais(
+    idPais INT NOT NULL AUTO_INCREMENT,
+    nombrePais VARCHAR(50) NOT NULL,
+    PRIMARY KEY(idPais)
+);
+CREATE TABLE departamento(
+    idDep INT NOT NULL AUTO_INCREMENT,
+    nombreDep VARCHAR(50) NOT NULL,
+    idPais INT,
+    PRIMARY KEY (idDep),
+    FOREIGN KEY (idPais) REFERENCES pais(idPais)
+);
+CREATE TABLE region(
+    idReg INT NOT NULL AUTO_INCREMENT,
+    nombreReg VARCHAR(50) NOT NULL,
+    idDep INT,
+    PRIMARY KEY (idReg),
+    FOREIGN KEY (idDep) REFERENCES departamento(idDep)
+);
+CREATE TABLE campers(
+    idCamper INT NOT NULL AUTO_INCREMENT,
+    nombreCamper VARCHAR(50) NOT NULL,
+    apellidoCamper VARCHAR(50) NOT NULL,
+    fechaNac DATE NOT NULL,
+    idReg INT,
+    PRIMARY KEY (idCamper),
+    FOREIGN KEY (idReg) REFERENCES region(idReg)
+);
+
+/*Observar las tablas de la base de datos */
+SHOW TABLES;
+
+/*Observar las columnas de cada tabla*/
+DESCRIBE pais;
+DESCRIBE departamento;
+DESCRIBE region;
+DESCRIBE campers;
